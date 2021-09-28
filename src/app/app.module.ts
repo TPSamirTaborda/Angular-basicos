97b8d60580d955a4a3c0e54c4d1744b668d8b760
Samir Taborda Sierra <tabordasierra.5@teleperformance.co>
2021-09-28 16:56:18 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
+import { ContadorModule } from './contador/contador.module';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+import { HeroesModule } from './heroes/heroes.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
